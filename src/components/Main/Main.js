e48e48e29410bb6b44334eabee2b2127df26f3a7
Aleksandr Nagaev <nagaevvvvv@yandex.ru>
2021-12-01 18:11:05 +0300

    feat: сверстать about project

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Main.css'
 import Header from '../Header/Header'
 import Promo from '../Promo/Promo'
-// import AboutProject from '../AboutProject/AboutProject'
+import AboutProject from '../AboutProject/AboutProject'
 // import Techs from '../Techs/Techs'
 // import AboutMe from '../AboutMe/AboutMe'
 // import Portfolio from '../Portfolio/Portfolio'
@@ -13,8 +13,8 @@ export default function Main() {
     <>
       <Header />
       <Promo />
-      {/* <AboutProject />
-      <Techs />
+      <AboutProject />
+      {/* <Techs />
       <AboutMe />
       <Portfolio />
       <Footer /> */}
