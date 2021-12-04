7363bcac4d2ab444f6a020d28d82fdcf4335f193
Aleksandr Nagaev <nagaevvvvv@yandex.ru>
2021-12-04 18:26:16 +0300

    feat: добавить роуты и компоненты

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,43 @@
-import "./App.css";
-import Main from "../Main/Main";
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import './App.css'
+import Main from '../Main/Main'
+import Movies from '../Movies/Movies'
+import SavedMovies from '../SavedMovies/SavedMovies'
+import Profile from '../Profile/Profile'
+import Register from '../Register/Register'
+import Login from '../Login/Login'
 
 function App() {
   return (
-    <>
-      <div className="App">
-        <div className="page">
-          <Main />
-        </div>
+    <div className="App">
+      <div className="page">
+        <Switch>
+          <Route path="/movies">
+            <Movies />
+          </Route>
+          <Route path="/saved-movies">
+            <SavedMovies />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/signup">
+            <Register />
+          </Route>
+          <Route path="/signin">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Main />
+          </Route>
+          <Route path="/*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
       </div>
-    </>
-  );
+    </div>
+  )
 }
 
-export default App;
+export default App
